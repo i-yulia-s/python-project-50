dfd1651a703d2a199599e477674b69007a353fce
i-yulia-s <i-yulia@mail.ru>
2025-04-12 22:42:02 +0400

    Add utility help using argparser

--- gendiff/scripts/main.py
+++ gendiff/scripts/main.py
@@ -2,7 +2,11 @@ from argparse import ArgumentParser
 
 
 def main():
-    parser = ArgumentParser()
+    parser = ArgumentParser(prog='gendiff', \
+        description='A utility that finds difference between two configuration files.')
+    parser.add_argument('first_file')
+    parser.add_argument('second_file')
+    parser.add_argument('-f', '--format', help='set format of output')
     parser.parse_args()
     print("Hello from python-project-50!")
 
